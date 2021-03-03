c9a8fc2a130fac291646bd98dc6433d0ac09d3d3
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2021-03-03 22:21:23 +0000

    Changed end of Life to Unix

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,6 +1,7 @@
 rm -rf "..out/"
 mkdir -p "../out/classes/client"
 mkdir -p "../out/classes/server"
+
 javac ../src/server/*.java -d ../out/classes/
 javac ../src/client/*.java -d ../out/classes/ -cp ../src/server/*.java
 echo "Finished compiling"
--- scripts/start-registry.sh
+++ scripts/start-registry.sh
@@ -1,5 +1,4 @@
 parentdir="$(dirname "$PWD")"
 echo "Parent directory: $parentdir"
-
 CLASSPATH="$parentdir/out/classes" export CLASSPATH
 rmiregistry #-J-Djava.rmi.server.codebase="file:$parentdir/out/classes/"
