ad6af84f41d40fe8cbc2f73a6f7bf690cb5731dc
98jmilton <98jmilton@gmail.com>
2021-03-05 23:13:09 +0000

    Update to compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,6 +2,6 @@ rm -rf "..out/"
 mkdir -p "../out/classes/client"
 mkdir -p "../out/classes/server"
 
-javac ../src/server/*.java -d ../out/classes/
+javac ../src/server/*.java ../src/server/errors/*.java -d ../out/classes/
 javac ../src/client/*.java -d ../out/classes/ -cp ../src/server/*.java
 echo "Finished compiling"
